c30b96ecb78f0042982e7d37de6776dd4b483c30
Santiago <bobadillasantiago00@gmail.com>
2023-07-11 18:26:03 -0300

    Se agrego en admin la clase de articulo, etiqueta y categoria

--- apps/articulos/admin.py
+++ apps/articulos/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Categoria, Etiqueta, Articulo
 
 # Register your models here.
+
+@admin.register(Articulo)
+class ArticulosAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'titulo' ,'bajada', 'fecha_actualiz', 
+                    'publicado', 'contenido', 'activo', 'categoria',
+                    'imagen', 'fecha_creacion', 'etiqueta')
+                    
+admin.site.register(Categoria)
+admin.site.register(Etiqueta)    
+    
+    
+     
+    
+   
