68cce987c4c9c0f5743d309afa2b487c04695c94
Santiago <bobadillasantiago00@gmail.com>
2023-07-11 17:42:49 -0300

    Se creo categoria

--- apps/articulos/models.py
+++ apps/articulos/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+#Categoria
+class Categoria(models.Model):
+
+    nombre = models.CharField(max_length=200, null=False)
+    activo = models.BooleanField(default=True)
+    creacion = models.DateTimeField(auto_now_add=True)
+    actualizacion = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.nombre
+
