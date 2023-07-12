dbaa69944672474c7c92e12e1cfbe988f65be4de
Ironwall72 <134495742+Ironwall72@users.noreply.git>
2023-07-12 08:37:25 -0300

    Se trabajo en blog/views y blog/urls

--- blog/urls.py
+++ blog/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import index # importamos desde views la funcion index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name= 'index'), # declaramos dentro de urlpatterns
 ]
