9afa6ab26976bef1d53eb1635147261b4ff562db
Rodrigo Murillas <rodrigojm_@hotmail.com>
2023-07-21 10:21:39 -0300

    html, views y urls para eliminar articulos agregados

--- apps/articulos/urls.py
+++ apps/articulos/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import PostListView, PostDetailView
+from .views import PostListView, PostDetailView, DeletePostView
 
+app_name = 'apps.posts'
 
 urlpatterns = [
     path('articulos/', PostListView.as_view(), name='articulos'),
     path('articulos/<int:id>/', PostDetailView.as_view(), name='articulo_ind'),
+    path('articulos/<int:id>/delete', DeletePostView.as_view(), name='delete_post'),
 ]
--- apps/articulos/views.py
+++ apps/articulos/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Articulo
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, DeleteView
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -15,3 +16,10 @@ class PostDetailView(DetailView):
     context_object_name = 'articulos'
     pk_url_kwarg = 'id'
     queryset = Articulo.objects.all()
+
+class DeletePostView(DeleteView):
+    model = Articulo
+    context_object_name = 'articulos'
+    template_name = 'post/delete_post.html'
+    pk_url_kwarg = 'id'
+    success_url = reverse_lazy('apps.posts:articulos')
