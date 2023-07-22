e62deed5a1004d37c385e5a3d8234e9baa2ee0ba
Dacktylos <dario.romerogreco@gmail.com>
2023-07-22 00:29:27 -0300

    Revert "Merge pull request #12 from Carolinasasowsky/rodrigo_eliminarpost"
    This reverts commit 862b7716a3e6eecfdb859fa7b49f34a7a7b64e97, reversing
    changes made to 717e1cd7636e272bac82357de990738381108c80.

--- blog/configuraciones/settings.py
+++ blog/configuraciones/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -56,7 +55,7 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(os.path.dirname(BASE_DIR),'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,12 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = (os.path.join(os.path.dirname(BASE_DIR),'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-MEDIA_URL ='/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),'media')
--- blog/urls.py
+++ blog/urls.py
@@ -15,18 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from .views import index # importamos desde views la funcion index
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name= 'index'), # declaramos dentro de urlpatterns
-    path('', include('apps.articulos.urls')),
-]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += staticfiles_urlpatterns()
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+    
+]
